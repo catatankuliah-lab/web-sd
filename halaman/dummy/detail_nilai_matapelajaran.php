74d28fbdc175be2a5a108cd455ef2fc31986bbd3
Nana <nanasupriatnakursus@gmail.com>
2021-11-29 01:25:50 +0700

    update home_guru.php and detail_nilai_mata[elajaran.php
    setup a href home_guru.php, create to databse table_nilai_matapelajaran when id_matapelajaran = 0

--- halaman/dummy/detail_nilai_matapelajaran.php
+++ halaman/dummy/detail_nilai_matapelajaran.php
@@ -1,4 +1,36 @@
 <?php
-include_once("../../php/connection.php");
-session_start();
+    include_once("../../php/connection.php");
+    session_start();
+    $id_url = $_GET['id'];
+    $kelas = $_SESSION['kelas'];
+    $query_matapelajaran = "SELECT * FROM table_nilai_matapelajaran WHERE id_matapelajaran=$id_url ORDER BY id_siswa ASC";
+    $result_query_matapelajaran = mysqli_query($conn, $query_matapelajaran);
+    $result_query_siswa = mysqli_query($conn, "SELECT * FROM table_siswa WHERE kelas=$kelas ORDER BY nama_siswa");
+    if($result_query_matapelajaran -> num_rows > 0) {
+?>
+        Sudah Input <br>
+        <table>
+            <tr>
+                <td>Nama Siswa</td>
+                <td>Action</td>
+            </tr>
+<?php
+            while($data_siswa = mysqli_fetch_array($result_query_siswa)) {         
+?>
+            <tr>
+                <td><?= $data_siswa['nama_siswa'] ;?></td>
+                <td><button>Update</button></td>
+            </tr>
+<?php 
+             }
+?>
+        </table>
+<?php
+    } else {
+        while($data_siswa = mysqli_fetch_array($result_query_siswa)) {         
+            $id_siswa = $data_siswa['id_siswa'];
+            mysqli_query($conn, "INSERT INTO table_nilai_matapelajaran (id_siswa, id_matapelajaran) VALUES('$id_siswa','$id_url')");
+        }
+        header("Refresh:0");
+    }
 ?>
--- halaman/dummy/home_guru.php
+++ halaman/dummy/home_guru.php
@@ -9,6 +9,7 @@
         $nama_guru = $data_guru['nama_guru'];
         $kelas = $data_guru['kelas'];
     }
+    $_SESSION['kelas'] = $kelas;
     $result_query_matapelajaran = mysqli_query($conn, "SELECT * FROM table_matapelajaran WHERE kelas=$kelas ORDER BY id_matapelajaran ASC");
     $result_query_jumlah_matapelajaran = mysqli_num_rows($result_query_matapelajaran);
     $result_query_siswa = mysqli_query($conn, "SELECT * FROM table_siswa WHERE kelas=$kelas ORDER BY nama_siswa");
